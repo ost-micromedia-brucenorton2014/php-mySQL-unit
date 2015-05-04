37ef37ad1f0f08ab3f77f1049e267336668f066b
brucenorton <bruce.norton@gmail.com>
2015-05-04 07:14:23 -0400

    updated select.php

--- select.php
+++ select.php
@@ -10,31 +10,26 @@
 <?php
 	include  '_includes/connect.php';
 	
-	//define and select table
-		$tblTest = "Bruce_app";//change to your table i.e. andreaTable
-		$displayQuery = "SELECT $tblTest.ID, $tblTest.userName, $tblTest.userEmail, $tblTest.userMessage FROM $tblTest";
-		
-		//get table info
-		if($displayStmt = $mysqli-> prepare($displayQuery)){
-			// execute prepared statement
+	//define table
+		$tbl = "Bruce_app";//change to your table i.e. John_app
+
+	//write query
+		$query = "SELECT $tbl.ID, $tbl.userName, $tbl.userEmail, $tbl.userMessage FROM $tbl";
+	//prepare statement, execute, store_result
+		if($displayStmt = $mysqli->prepare($query)){
 			$displayStmt->execute();
 			$displayStmt->store_result();
 			$numrows = $displayStmt->num_rows;
-			echo ("<br/>results: $numrows<br/>");
-			
-			 // bind results
-			 $displayStmt->bind_result($idResult, $usernameResult, $userEmailResult, $userMessageResult);
-			
-			// fetch comments
-			while ($displayStmt->fetch()) {
-				echo("$idResult, $usernameResult $userEmailResult, $userMessageResult<br>");
-				
-			}//end while
-		}else{
-			echo("<br>Error description: " . mysqli_error($mysqli));
-			
+			echo("<br>results: $numrows");
+
 		}
-	echo("<br>displayStmt error: ".$displayStmt->error);
+	//bind results
+		$displayStmt->bind_result($IDResult, $nameResult, $emailResult, $messageResult);
+
+	//fetch results
+		while($displayStmt->fetch()){
+			echo("<br>results: $IDResult, $nameResult, $emailResult, $messageResult");
+		}
 ?>
 
 </body>
